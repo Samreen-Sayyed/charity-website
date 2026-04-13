aef08b0b6df4e4e1e1122f495922e63440286ed4
Samreen-Sayyed <sayyedsamreen125@gmail.com>
2026-04-13 14:01:07 +0530

    local chnages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-
 import Home from "./components/Home/Home";
 import Contact from './components/Contact/Contact';
 import About from './components/About/About';
@@ -10,13 +9,12 @@ import Projects from './components/Projects/Projects';
 import Scholarship from './components/Scholarship/Scholarship';
 import MemberType from './components/MemberType/MemberType';
 import MemberData from './components/MemberData/MemberData';
-
+import Founders from './components/Founders/Founders';
 
 function App() {
   return (
     <div className="app-container">
       <BrowserRouter>
-        
         <Navbar />
 
         <div className="content">
@@ -25,20 +23,17 @@ function App() {
             <Route path="/home" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/about" element={<About />} />
+            <Route path="/founders" element={<Founders />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/scholarship" element={<Scholarship />} />
             <Route path="/member-type" element={<MemberType />} />
             <Route path="/member-data" element={<MemberData />} />
-           
-
           </Routes>
         </div>
 
         <Footer />
-
       </BrowserRouter>
     </div>
   );
 }
-
 export default App;
