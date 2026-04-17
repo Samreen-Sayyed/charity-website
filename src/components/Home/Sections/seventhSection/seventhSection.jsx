79cc908614a8708aceec1f165c45965700de7aca
naikchirag227-ship-ds <naikchirag227@gmail.com>
2026-04-17 15:03:32 +0530

    Fixed ESLint warnings and finalized Formspree integration

--- src/components/Home/Sections/seventhSection/seventhSection.jsx
+++ src/components/Home/Sections/seventhSection/seventhSection.jsx
@@ -48,12 +48,12 @@ function SeventhSection() {
                     <div className='ninth-lower-first-div' data-aos="zoom-in">
                         <img src={UpperPic1} alt="blog" />
                         <h2>
-                            <a href="#">Giving The Poor Is A Great Satisfaction Of heart.</a>
+                            <a href="/">Giving The Poor Is A Great Satisfaction Of heart.</a>
                         </h2>
                         <hr />
                         <div className='ninth-section-comments'>
                             <img src={LowerPic1} alt="author" />
-                            <p><a href="#">Jenefer Willy</a></p>
+                            <p><a href="/">Jenefer Willy</a></p>
                             <p id='ninth-section-comments-template'>
                                 <BsChatText /> 35
                             </p>
@@ -64,12 +64,12 @@ function SeventhSection() {
                     <div className='ninth-lower-second-div' data-aos="zoom-in" data-aos-delay="200">
                         <img src={UpperPic2} alt="blog" />
                         <h2>
-                            <a href="#">Your Help Can Make Someone’s life Easier.</a>
+                            <a href="/">Your Help Can Make Someone’s life Easier.</a>
                         </h2>
                         <hr />
                         <div className='ninth-section-comments'>
                             <img src={LowerPic2} alt="author" />
-                            <p><a href="#">Laura Falary</a></p>
+                            <p><a href="/">Laura Falary</a></p>
                             <p id='ninth-section-comments-template'>
                                 <BsChatText /> 80
                             </p>
@@ -80,12 +80,12 @@ function SeventhSection() {
                     <div className='ninth-lower-third-div' data-aos="zoom-in" data-aos-delay="400">
                         <img src={UpperPic3} alt="blog" />
                         <h2>
-                            <a href="#">Give Education, It’s The Best Gift Ever.</a>
+                            <a href="/">Give Education, It’s The Best Gift Ever.</a>
                         </h2>
                         <hr />
                         <div className='ninth-section-comments'>
                             <img src={LowerPic3} alt="author" />
-                            <p><a href="#">Laura Falary</a></p>
+                            <p><a href="/">Laura Falary</a></p>
                             <p id='ninth-section-comments-template'>
                                 <BsChatText /> 95
                             </p>
